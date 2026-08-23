9e19780dc9b59d39356edf694476a575ef589523
kit <nguyenthuylinh321997@gmail.com>
2026-08-23 21:43:24 +0700

    tamgiac

---
+++ java.js
@@ -0,0 +1,16 @@
+function tamgiac(h) {
+    for (let i = 0; i <= h; i++) {
+        let row = " * ";
+        let space = "";
+        for (let k = 1; k <= h-i ; k++) { 
+
+            space += "  ";
+        }
+        console.log(space+row);
+
+    }
+
+}
+tamgiac(4
+    
+)
